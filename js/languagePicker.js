0efc9f914c228fa0e758031d439bfecb6ae727dd
Tianheng Wei <weitianheng@outlook.de>
2022-07-31 17:51:47 +0200

    debug

--- js/languagePicker.js
+++ js/languagePicker.js
@@ -1,12 +1,12 @@
 let searchParams = new URL(document.location).searchParams;
 var language_g = searchParams.get("language") ?? "en";
-$(`#lang_pcker > .${language_g}`).addClass("active");
+$(`#lang_picker > .${language_g}`).addClass("active");
 
 $("#lang_picker").click(() => {
-  $("#lang_pcker > span").toggleClass("active");
+  $("#lang_picker > span").toggleClass("active");
   let params = new URLSearchParams(searchParams);
   params.set("language", language_g === "en" ? "de" : "en");
   let paramsStr = params.toString();
-  let newURL = document.location.href.split["?"][0] + paramsStr;
+  let newURL = document.location.href.split("?")[0] + paramsStr;
   window.open(newURL);
 });
